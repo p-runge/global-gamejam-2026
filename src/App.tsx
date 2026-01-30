47c1a9c8628a56fa50f430b56854ad3996449cfe
Luke Dowling <lpdowling26@googlemail.com>
2026-01-30 20:01:50 +0100

    feat: add start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { createRoot } from "react-dom/client";
-import { Canvas } from "@react-three/fiber";
+import "./App.css"
+
+import { createRoot } from "react-dom/client"
+import { Canvas } from "@react-three/fiber"
+
+import { useEffect, useState } from "react"
 
 export default function App() {
-  return (
-    <div id="canvas-container">
+  const [isPlaying, setIsPlaying] = useState(false)
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === " ") {
+        setIsPlaying(true)
+      }
+    })
+  }, [])
+
+  return isPlaying ? (
+    <div id='canvas-container'>
       <Canvas>
         <mesh>
           <boxGeometry />
@@ -11,7 +24,11 @@ export default function App() {
         </mesh>
       </Canvas>
     </div>
-  );
+  ) : (
+    <div className='start'>
+      <p>Press space to start</p>
+    </div>
+  )
 }
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />)
