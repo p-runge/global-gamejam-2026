afb8fda965d00630dc158f770e22c7725cf251d3
Marvin Demirkaya <m.demirkaya@progani.com>
2026-01-30 22:53:39 +0100

    add icons to ui

--- src/ui.tsx
+++ src/ui.tsx
@@ -3,17 +3,16 @@ import UIElement from "./components/ui-element";
 export default function UI() {
   return (
     <UIElement>
-      <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          pointerEvents: "none",
-        }}
-        className="border rounded-lg bg-white text-blue-500 flex flex-col p-2 m-4"
-      >
-        <p>Level: 69</p>
-        <p>Score: over 9000</p>
+      <div className="flex justify-between">
+        <div className="border rounded-lg bg-white text-blue-500 flex flex-col p-2 m-4">
+          <p>Level: 69</p>
+          <p>Score: over 9000</p>
+        </div>
+      </div>
+      <div className="border rounded-lg bg-white text-red-500 flex p-2 m-4">
+        <div>Icon 1</div>
+        <div>Icon 2</div>
+        <div>Icon 3</div>
       </div>
     </UIElement>
   );
