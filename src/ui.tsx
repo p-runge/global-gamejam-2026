81443f3e198566cd2111f02ec7e6a3e494698e14
Marvin Demirkaya <m.demirkaya@progani.com>
2026-01-30 21:26:48 +0100

    position in ui

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,5 +1,18 @@
-import { Text } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 
 export default function UI() {
-  return <Text children="abc"></Text>;
+  return (
+    <Html position={[0, 0, 0]} transform={false} fullscreen>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          pointerEvents: "none",
+        }}
+      >
+        abc
+      </div>
+    </Html>
+  );
 }
