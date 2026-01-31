6ebcaa5bafdcd3a8800a53667b93db195afb40b0
Philipp Runge <p.runge@progani.com>
2026-01-31 11:50:36 +0100

    improve collision accuracy

--- src/utils/collision.tsx
+++ src/utils/collision.tsx
@@ -1,11 +1,7 @@
-import { Sphere, Box3, Object3D } from "three";
+import { Box3, Object3D } from "three";
 
 export function isColliding(object1: Object3D, object2: Object3D): boolean {
   const box1 = new Box3().setFromObject(object1);
   const box2 = new Box3().setFromObject(object2);
-  const sphere1 = new Sphere();
-  const sphere2 = new Sphere();
-  box1.getBoundingSphere(sphere1);
-  box2.getBoundingSphere(sphere2);
-  return sphere1.intersectsSphere(sphere2);
+  return box1.intersectsBox(box2);
 }
